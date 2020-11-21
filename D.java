a23f5aba75d59a329a21581281d166a2f7a91894
whoishenriquexd <adilson.henrique@hotmail.com>
2020-11-21 20:43:30 -0300

    Criacao da Classe D

---
+++ D.java
@@ -0,0 +1,39 @@
+/* Nome: Adilson Henrique de Oliveira Lima
+Matricula: 201711967
+Turma: 10A */
+
+public class D {
+    
+    private string D1;
+    private int D2;
+    
+    public D(string mD1, int mD2){
+        this.D1 = mD1;
+        this.D2 = mD2;
+    }
+    
+    public string getD1(){
+    	return D1;
+    }
+
+	public int getD2(){
+    	return D2;
+    }
+    
+    public void setC1(int D1) {
+		this.D1 = D1;
+	}
+
+	public void setD2(float D2) {
+		this.D2 = D2;
+    
+    public void MD1(){
+    	System.out.println("MD1");
+  	}
+  	
+  	public void MD2(){
+    	System.out.println("MD2");
+  	}
+  	
+}
+
