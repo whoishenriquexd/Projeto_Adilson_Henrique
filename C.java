f12a56357a409ceec096bbaca9e0cc7aad484362
whoishenriquexd <adilson.henrique@hotmail.com>
2020-11-21 19:39:03 -0300

    Criacao da Classe C

---
+++ C.java
@@ -0,0 +1,39 @@
+/* Nome: Adilson Henrique de Oliveira Lima
+Matricula: 201711967
+Turma: 10A */
+
+public class C {
+    
+    private string C1;
+    private int C2;
+    
+    public B(string mC1, int mC2){
+        this.C1 = mC1;
+        this.C2 = mC2;
+    }
+    
+    public string getC1(){
+    	return C1;
+    }
+
+	public int getC2(){
+    	return C2;
+    }
+    
+    public void setC1(int C1) {
+		this.C1 = C1;
+	}
+
+	public void setC2(float C2) {
+		this.C2 = C2;
+    
+    public void MC1(){
+    	System.out.println("MC1");
+  	}
+  	
+  	public void MC2(){
+    	System.out.println("MC2");
+  	}
+  	
+}
+
