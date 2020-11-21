6411bbb6726c6832585d39fdd1a108b4eddbd81d
Adilson Henrique de Oliveira Lima <58189823+whoishenriquexd@users.noreply.github.com>
2020-11-21 16:35:40 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,38 @@
+/* Nome: Adilson Henrique de Oliveira Lima
+Matricula: 201711967
+Turma: 10A */
+
+public class A {
+    
+    private int A1;
+    private float A2;
+    
+    public A(int mA1, float mA2){
+        this.A1 = mA1;
+        this.A2 = mA2;
+    }
+    
+    public int getA1(){
+    	return A1;
+    }
+
+	public float getA2(){
+    	return A2;
+    }
+    
+    public void setA1(int A1) {
+		this.A1 = A1;
+	}
+
+	public void setA2(float A2) {
+		this.A2 = A2;
+    
+    public void MA1(){
+    	System.out.println("MA1");
+  	}
+  	
+  	public void MA2(){
+    	System.out.println("MA2");
+  	}
+}
+
---
+++ B.java
@@ -0,0 +1,39 @@
+/* Nome: Adilson Henrique de Oliveira Lima
+Matricula: 201711967
+Turma: 10A */
+
+public class B {
+    
+    private int B1;
+    private float B2;
+    
+    public B(int mB1, float mB2){
+        this.B1 = mB1;
+        this.B2 = mB2;
+    }
+    
+    public int getB1(){
+    	return B1;
+    }
+
+	public float getB2(){
+    	return B2;
+    }
+    
+    public void setB1(int B1) {
+		this.B1 = B1;
+	}
+
+	public void setB2(float B2) {
+		this.B2 = B2;
+    
+    public void MB1(){
+    	System.out.println("MB1");
+  	}
+  	
+  	public void MB2(){
+    	System.out.println("MB2");
+  	}
+    
+}
+
